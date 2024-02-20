5e85d4f54ecd5210759ca70e8887098ce7d0b29f
Abodawoud <yousefabodawoud@gmail.com>
2024-02-20 14:11:25 +0200

    auth

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -10,11 +10,21 @@ from typing import TypeVar
 
 class Auth:
     """
-      Auth Class
+        Auth Class
     """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Require Auth"""
-        return False
+        if path is None:
+            return True
+
+        if excluded_paths is (None or []):
+            return True
+
+        for excluded_path in excluded_paths:
+            if path.startswith(excluded_path.rstrip('/')):
+                return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         """Authorization Header"""
