fb423a31afdd87aa9d568fb1770ca11f55efa075
Abodawoud <yousefabodawoud@gmail.com>
2024-02-20 14:14:19 +0200

    auth

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -17,7 +17,7 @@ class Auth:
         if path is None:
             return True
 
-        if excluded_paths is (None or []):
+        if excluded_paths is None or len(excluded_paths) == 0:
             return True
 
         for excluded_path in excluded_paths:
