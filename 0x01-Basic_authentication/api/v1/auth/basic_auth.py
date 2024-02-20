c308ceda9992fe91c5a23e027cf0121caf3b05ee
Abodawoud <yousefabodawoud@gmail.com>
2024-02-20 15:28:52 +0200

    base64

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -25,3 +25,23 @@ class BasicAuth(Auth):
         if not authorization_header.startswith("Basic "):
             return None
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(
+        self, base64_authorization_header: str
+    ) -> str:
+        """"
+            decode base64 authorization header
+        """
+        if base64_authorization_header is None:
+            return None
+
+        if not isinstance(base64_authorization_header, str):
+            return None
+
+        try:
+            decoded_bytes = base64.b64decode(
+                base64_authorization_header)  # base64 to bytes
+            decoded_string = decoded_bytes.decode('utf-8')  # bytes to str
+            return decoded_string
+        except Exception as e:
+            return None
